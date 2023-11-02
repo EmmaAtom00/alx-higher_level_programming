0d5d2849c3933144b173cd62e303644ef3274e84
Emmanuel <emmanuelmark501@gmail.com>
2023-11-02 14:02:38 -0700

    printing args and their number

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """printing command line arguments"""
-    import sys
+    from sys import argv
 
-    length = len(sys.argv) - 1
-    if length == 0:
-        print("0 arguments.")
-    elif length == 1:
-        print("1 arguments:")
+    count = len(argv)
+    delim = ":"
+
+    if count == 1:
+        delim = "."
+        count = 0
+        print("{} arguments{}".format(count, delim))
+    elif count == 2:
+        print("{} argument{}".format(count - 1, delim))
     else:
-        print("{} arguments:".format(length))
-    for i in range(length):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments{}".format(count - 1, delim))
+
+    for i in range(1, count):
+        print("{}{} {}".format(i, delim, argv[i]))
