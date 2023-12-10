dceb2a22c7457439583c33e4838ef5170f217ded
Emmanuel <emmanuelmark501@gmail.com>
2023-12-10 07:09:11 -0800

    multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    stringLength = len(sentence)
-    firstCharacter = sentence[0]
     if sentence == '':
         firstCharacter = None
+    else:
+        firstCharacter = sentence[0]
+    stringLength = len(sentence)
     returnTuple = (stringLength, firstCharacter)
     return returnTuple
