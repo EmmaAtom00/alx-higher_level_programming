389b3a9b1b5f7e3e6e04e427aa0ef4182d692147
Emmanuel <emmanuelmark501@gmail.com>
2023-12-10 22:45:15 -0800

    square with a size attribbute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Declaring a class with a private attribute size"""
+
+class Square:
+    """The square object and attributes goes here"""
+
+    def __init__(self, size):
+        """
+            Initialize a new size
+            Args:
+                size (int): new size of the square
+        """
+        self.__size = size
