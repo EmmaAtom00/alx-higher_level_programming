803269f15c2ebd3824c372b50d08a514e0f3bd99
Emmanuel <emmanuelmark501@gmail.com>
2024-05-27 11:22:53 -0700

    ValueError exception

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
+    Args:
+        value (int): the integer to be printed
+    Return:
+        Return true if value has been correctly printed, otherwise false
+    """
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        print("{} is not an integer".format(value))
+        return False
