f7838ade4db11c56a5eb379dfefeb55c31d0c73c
Emmanuel <emmanuelmark501@gmail.com>
2024-05-31 23:13:34 -0700

    using getters and setters in a class

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,22 +7,28 @@ class Square:
 
     def __init__(self, size=0):
         """Initialization of the object"""
+
         if not isinstance(size, int):
-            return TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
+
         elif size < 0:
-            return ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
+
         self.__size = size
 
     @property
     def size(self):
         """Using getter to retrieve the private attribute"""
+
         return (self.__size)
 
     @size.setter
     def size(self, value):
         """Using setter to change the value of the priate attribute"""
+
         self.__size = value
 
     def area(self):
         """Public method that returns the area of the square"""
+
         return (self.__size * self.__size)
